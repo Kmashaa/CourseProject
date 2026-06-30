0a0108148ad84dde3b652767a164b23f17a722ad
Kmashaa <m.kravcenko@yandex.by>
2026-07-01 00:50:54 +0300

    2. Implementation of the Booking entity

---
+++ CourseProject/Data/BookingRepository.cs
@@ -0,0 +1,60 @@
+﻿using CourseProject.Entities;
+using CourseProject.Interfaces;
+
+namespace CourseProject.Data
+{
+    public class BookingRepository : IBookingRepository
+    {
+        private readonly List<Booking> _bookings = new();
+
+        public BookingRepository()
+        {
+        }
+
+        public List<Booking> GetAll()
+        {
+            return _bookings;
+        }
+
+        public Booking? GetById(Guid id)
+        {
+            return _bookings.FirstOrDefault(o => o.Id == id);
+        }
+
+        public Booking Create(Booking booking)
+        {
+            booking.Id = Guid.NewGuid();
+            booking.Status = BookingStatus.Pending;
+            booking.CreatedAt = DateTime.Now;
+
+            _bookings.Add(booking);
+            return booking;
+        }
+
+        public Booking? Update(Booking booking)
+        {
+            var index = _bookings.FindIndex(o => o.Id == booking.Id);
+            if (index != -1)
+            {
+                _bookings[index] = booking;
+                return _bookings[index];
+            }
+
+            return null;
+
+        }
+
+        public bool Delete(Guid id)
+        {
+            var eventFromList = _bookings.FirstOrDefault(o => o.Id == id);
+
+            if (eventFromList != null)
+            {
+                _bookings.Remove(eventFromList);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ CourseProject/Interfaces/IBookingRepository.cs
@@ -0,0 +1,18 @@
+﻿using CourseProject.Entities;
+
+namespace CourseProject.Interfaces
+{
+    public interface IBookingRepository
+    {
+        List<Booking> GetAll();
+
+        Booking? GetById(Guid id);
+
+        Booking Create(Booking booking);
+
+        Booking? Update(Booking booking);
+
+        bool Delete(Guid id);
+
+    }
+}
