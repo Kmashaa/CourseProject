4e8c00818586d9361587c8f2f3e1aefde851022f
Kmashaa <m.kravcenko@yandex.by>
2026-08-18 21:48:05 +0300

    7. Updating tests

--- CourseProject.Application/Services/UserService.cs
+++ CourseProject.Application/Services/UserService.cs
@@ -1,37 +1,26 @@
 ﻿using CourseProject.Application.Interfaces;
-using CourseProject.Application.Models;
 using CourseProject.Domain.Entities;
 using CourseProject.Domain.Exceptions;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace CourseProject.Application.Services
 {
-    public class UserService: IUserService
+    public class UserService : IUserService
     {
 
         private readonly IPasswordHasher _passwordHasher;
         private readonly IUserRepository _userRepository;
         private readonly IJwtTokenGenerator _jwtTokenGenerator;
 
-        //private readonly IBookingDtoMapperService _bookingDtoMapperService;
-
-
         public UserService(IPasswordHasher passwordHasher, IUserRepository userRepository, IJwtTokenGenerator jwtTokenGenerator)
         {
             _passwordHasher = passwordHasher;
             _userRepository = userRepository;
             _jwtTokenGenerator = jwtTokenGenerator;
-            //_bookingRepository = bookingRepository;
-            //_eventRepository = eventRepository;
-            //_eventDtoMapperService = eventDtoMapperService;
-            //_bookingDtoMapperService = bookingDtoMapperService;
         }
 
         public async Task<Guid> RegisterUserAsync(string login, string password, string role)
         {
-            if (Enum.TryParse<Domain.Entities.Roles>(role, true, out Domain.Entities.Roles enumRole)) 
+            if (Enum.TryParse<Domain.Entities.Roles>(role, true, out Domain.Entities.Roles enumRole))
             {
                 var passwordHash = _passwordHasher.Hash(password);
                 var user = User.Create(login, passwordHash, enumRole);
