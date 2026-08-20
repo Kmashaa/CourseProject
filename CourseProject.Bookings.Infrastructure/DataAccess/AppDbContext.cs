224f8680098c543cc47731202d287d88dd93d973
Kmashaa <m.kravcenko@yandex.by>
2026-08-21 01:03:06 +0300

    final renaming. Add migrations

---
+++ CourseProject.Bookings.Infrastructure/DataAccess/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CourseProject.Bookings.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using CourseProject.Bookings.Domain.Entities;
+
+namespace CourseProject.Bookings.Infrastructure.DataAccess
+{
+    public sealed class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Booking> Bookings => Set<Booking>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+    }
+}
